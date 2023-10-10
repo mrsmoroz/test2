52accc5baeb53ee02efd2f87a1f0445192765719
Morozevich Maria <mrsmoroz21@gmail.com>
2023-10-11 01:17:35 +0300

    Add comment about last name

--- askname.c
+++ askname.c
@@ -5,6 +5,7 @@ void askname(char *first, char *last)
 printf("Enter your first name: ");
 fgets(first, 255, stdin);
 first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+/*getting last name*/
 printf("Now enter your last name: ");
 gets(last); /* buffer overflow? what's that? */
 }
