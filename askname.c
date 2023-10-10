997fb7ab1ca285a3e4c2c400d835cd7f7082fc8c
Natasha <natashaca@gmail.com>
2023-10-11 01:19:15 +0300

    Add comment about first name.

--- askname.c
+++ askname.c
@@ -2,6 +2,7 @@
 #include <string.h>
 void askname(char *first, char *last)
 {
+/*getting first name*/
 printf("Enter your first name: ");
 fgets(first, 255, stdin);
 first[strlen(first)-1] = '\0'; /* remove the newline at the end */
